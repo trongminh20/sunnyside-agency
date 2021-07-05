5b73bce3265119e7b8ccb6f3b2f3355f57df2010
Minh Phan <trongminh20@gmail.com>
2021-07-05 01:22:18 -0700

    toggle menu

---
+++ mainJS.js
@@ -0,0 +1,9 @@
+function toggleMenu(){
+  let menu = document.getElementById('toggle-menu');
+  if(menu.style.display != "none"){
+    menu.style.display = "none";
+  }else{
+    menu.style.display = "flex";
+
+  }
+}
